c16ce12f1dde5a31cf884abc93d58f898571b8d6
Eric Rohrs <rohrs.eric@gmail.com>
2017-01-05 17:26:22 +0000

    Added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :plan
-  
+  has_one :profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
