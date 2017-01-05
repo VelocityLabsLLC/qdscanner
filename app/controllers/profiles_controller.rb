9acd1ef7fc24144af7509e7bdba769e392d1b437
Eric Rohrs <rohrs.eric@gmail.com>
2017-01-05 18:20:01 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   # GET to /users/:user_id/profile/new
   def new
     @profile = Profile.new
@@ -16,8 +19,35 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find ( params[:user_id] )
+    @profile = @user.profile
+  end
+  
+  def update
+    # Retrieve the user from the database
+    @user = User.find ( params[:user_id] )
+    # Retrieve that users profile
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save (update)
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+  
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
